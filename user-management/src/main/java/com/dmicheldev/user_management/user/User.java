757c9db506f2136a495abd803a7c3096625246ef
D-Michel-dev <d.michel.dev@gmail.com>
2026-04-05 15:22:01 -0500

    feat: add user entity and configure postgresql connection

---
+++ user-management/src/main/java/com/dmicheldev/user_management/user/User.java
@@ -0,0 +1,39 @@
+package com.dmicheldev.user_management.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private UserEnum role;
+}
---
+++ user-management/src/main/java/com/dmicheldev/user_management/user/UserEnum.java
@@ -0,0 +1,6 @@
+package com.dmicheldev.user_management.user;
+
+public enum UserEnum {
+    USER,
+    ADMIN
+}
