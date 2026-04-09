9b3b65a21f4ec8871364ba9d48e15ebf7eb927df
D-Michel-dev <d.michel.dev@gmail.com>
2026-04-09 14:17:40 -0500

    feat: implement securityfilterchain

---
+++ user-management/src/main/java/com/dmicheldev/user_management/security/SecurityConfigurations.java
@@ -0,0 +1,36 @@
+package com.dmicheldev.user_management.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.POST, "/api/users/register").permitAll()
+            .requestMatchers(HttpMethod.POST,"/api/user/login").permitAll()
+            .requestMatchers(HttpMethod.GET, "/api/users").hasRole("ADMIN")
+            .anyRequest().authenticated()
+        )
+        .httpBasic(httpBasic -> {})
+        .build();
+    }
+}
