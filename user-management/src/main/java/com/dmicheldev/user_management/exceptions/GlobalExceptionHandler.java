6a4a3fa6960582e13c164e9b73d976af1d5dd6b9
D-Michel-dev <d.michel.dev@gmail.com>
2026-04-13 14:28:09 -0500

    feat: implement global exception handler

---
+++ user-management/src/main/java/com/dmicheldev/user_management/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,87 @@
+package com.dmicheldev.user_management.exceptions;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.dmicheldev.user_management.user.exceptions.BlankNameException;
+import com.dmicheldev.user_management.user.exceptions.EmailAlreadyExistsException;
+import com.dmicheldev.user_management.user.exceptions.ForbiddenException;
+import com.dmicheldev.user_management.user.exceptions.InvalidCredentialsException;
+import com.dmicheldev.user_management.user.exceptions.InvalidEmailException;
+import com.dmicheldev.user_management.user.exceptions.InvalidPasswordException;
+import com.dmicheldev.user_management.user.exceptions.UserNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
+
+    private static final String UNEXPECTED_ERROR_MESSAGE = "An unexpected error occurred. Try again later";
+
+    @ExceptionHandler(BlankNameException.class)
+
+    public ResponseEntity<ErrorResponse> handleBlankName(BlankNameException exception, HttpServletRequest request){
+        return buildError(HttpStatus.BAD_REQUEST, exception.getMessage(), request);
+    }
+
+    
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyExists(EmailAlreadyExistsException exception, HttpServletRequest request){
+        return buildError(HttpStatus.CONFLICT, exception.getMessage(), request);
+    }
+    
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity<ErrorResponse> handleForbidden(ForbiddenException exception, HttpServletRequest request){
+        return buildError(HttpStatus.FORBIDDEN, exception.getMessage(), request);
+    }
+    
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidCredential(InvalidCredentialsException exception, HttpServletRequest request){
+        return buildError(HttpStatus.BAD_REQUEST, exception.getMessage(), request);
+    }
+    
+    @ExceptionHandler(InvalidEmailException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidEmail(InvalidEmailException exception, HttpServletRequest request){
+        return buildError(HttpStatus.BAD_REQUEST, exception.getMessage(), request);
+    }
+    
+    @ExceptionHandler(InvalidPasswordException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidPassword(InvalidPasswordException exception, HttpServletRequest request){
+        return buildError(HttpStatus.BAD_REQUEST, exception.getMessage(), request);
+    }
+    
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException exception, HttpServletRequest request) {
+        return buildError(HttpStatus.NOT_FOUND, exception.getMessage(), request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception exception, HttpServletRequest request) {
+        return buildError(HttpStatus.INTERNAL_SERVER_ERROR, UNEXPECTED_ERROR_MESSAGE, request);
+    }
+
+    //--------------------------------------
+    //------------HELPER METHODS------------
+    //--------------------------------------
+
+    private ResponseEntity<ErrorResponse> buildError(
+        HttpStatus status, 
+        String message, 
+        HttpServletRequest request
+    ) { 
+        ErrorResponse errorResponse = new ErrorResponse(
+                status.value(), 
+                status.getReasonPhrase(), 
+                message, 
+                request.getRequestURI(), 
+                LocalDateTime.now()
+            );
+            return ResponseEntity.status(status).body(errorResponse);
+    }
+}
